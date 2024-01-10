a22e8105f11c3dc232962543b5f02f8b6016ce46
wangjie11 <1119792237@qq.com>
2024-01-10 18:41:39 +0800

    add action

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig(() => {
   return {
-    base: "/test-action",
+    base: "/",
     publicDir: "public",
     plugins: [react()],
   };
